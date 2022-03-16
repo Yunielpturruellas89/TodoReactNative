503db9dd59e3008b87fac0b6c56c2a341272fed4
yunielpt <yunielpturruellas@gmail.com>
2022-03-16 05:16:51 -0500

    Se agrega el proyecto

---
+++ App.js
@@ -0,0 +1,50 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <View>
+        <Text style={styles.container}>Ezequieljhkjhkjh </Text>
+        </View>
+      
+    </SafeAreaView>
+    
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#E0e0e0',
+    padding: 15,
+  },
+});
+
+export default App;
